f6201daec17ba92badbe3b1fa6d111118842be33
joostburgers <joostburgers@gmail.com>
2022-04-12 15:49:28 -0400

    Reset all variables

--- custom.js
+++ custom.js
@@ -1,11 +1,11 @@
 // JavaScript for enabling the map on load.
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9vc3RidXJnZXJzIiwiYSI6ImNqZmZjaGtzaDRrMncycXBrazdzNzFzZXUifQ.7MZmVPD9cq_QtgpzkcZPyA';
+mapboxgl.accessToken = 'Change HERE';
 
 window.onload = function() {
   const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/joostburgers/cl1w8a046000q14rrrewcwdem', // style URL
+    style: 'Change HERE', // style URL
     center: [77.0688997, 20.5272803], // starting position [lng, lat]
     zoom: 4 // starting zoom
   });
