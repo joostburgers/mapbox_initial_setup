352bab955055d21acd7b1def7430a262057b428e
joostburgers <joostburgers@gmail.com>
2022-04-12 15:23:21 -0400

    Added the API Key to the JS file

--- custom.js
+++ custom.js
@@ -1,11 +1,11 @@
 // JavaScript for enabling the map on load.
 
-mapboxgl.accessToken = 'change HERE';
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9vc3RidXJnZXJzIiwiYSI6ImNqZmZjaGtzaDRrMncycXBrazdzNzFzZXUifQ.7MZmVPD9cq_QtgpzkcZPyA';
 
 window.onload = function() {
   const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'change HERE', // style URL
+    style: 'mapbox://styles/joostburgers/cl1w8a046000q14rrrewcwdem', // style URL
     center: [77.0688997, 20.5272803], // starting position [lng, lat]
     zoom: 4 // starting zoom
   });
